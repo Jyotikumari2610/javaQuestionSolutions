eefe9204e6d548835b1235e49f05a38f8382daa4
Jyoti Kumari <jk1076387@gmail.com>
2026-02-28 21:57:38 +0530

    Add binary search implementation in Practice5.java
    Implement binary search to find target in array.

--- Practice5.java
+++ Practice5.java
@@ -260,3 +260,43 @@ Output:-
     15
     16
 === Code Execution Successful ===  
+
+//floor of a number
+    import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int target=sc.nextInt();
+        int strt=0;
+        int end=arr.length-1;
+        while(strt<=end){
+            int mid=strt+(end-strt)/2;
+            if(arr[mid]==target){
+                System.out.println(mid);
+            }
+            else if(arr[mid]<target){
+                strt=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        System.out.println(arr[end]);
+    }
+}
+Output:-
+    5
+45
+47
+49
+51
+53
+48
+47
+
+=== Code Execution Successful ===
